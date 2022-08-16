9e22b1a3dba83c42878196ea44d7e3183acd06f1
Israil <esrailman445@gmail.com>
2022-08-16 21:00:16 +0600

    Assignment 3 all commited

---
+++ Constants.java
@@ -0,0 +1,22 @@
+public class Constants {
+
+    public static final String showAll = "a";
+    public static final String loadingData = "Loading data ...";
+    public static final String loadData = "Data Loaded.";
+    public static final String random = "r";
+    public static final String comma = ",";
+    public static final String plus = "+";
+    public static final String dateFormate = "dd/mm/yyyy-hh:mm:ss a";
+    public static final String lastupdate = "\nList last updated on ";
+    public static final String question = "?";
+    public static final String found = "We found it!";
+    public static final String c = "c";
+    public static final String wordFound = " word(s) found ";
+    public static final String studentList= "students.txt";
+
+
+
+
+
+    
+}
--- StudentList.java
+++ StudentList.java
@@ -5,88 +5,84 @@ public class StudentList {
 	public static void main(String[] args) {
 
 //		Check arguments
-		if(args[0].equals("a")) {
-			System.out.println("Loading data ...");			
+		if(args[0].equals(Constants.showAll)) {
+			System.out.println(Constants.loadingData);			
 			try {
-				BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt")));
+				BufferedReader reader = read();
 			    String allStudent = reader.readLine();
 			    String names[] = allStudent.split(",");
 			    for(String name : names) {
 					System.out.println(name.trim());
 				}
 
-			} catch (Exception e){
+			} catch (Exception e) {
 				System.out.println(e);
 			}
-			System.out.println("Data Loaded.");
+			System.out.println(Constants.loadData);
 		}
 
-		else if(args[0].equals("r")) 
-		{
-			System.out.println("Loading data ...");			
+		else if(args[0].equals(Constants.random)) {
+			System.out.println(Constants.loadingData);			
 			try {
-				BufferedReader reader= new BufferedReader(new InputStreamReader(new FileInputStream("students.txt")));
+				BufferedReader reader = read();
 			    String allStudent = reader.readLine();
 		    	System.out.println(allStudent);
-		    	String names[] = allStudent.split(",");
+		    	String names[] = allStudent.split(Constants.comma);
 			    Random rand= new Random();
-			    int rand_index = rand.nextInt(4);
+			    int rand_index = rand.nextInt(names.length);
 				System.out.println(names[rand_index].trim());
-			} catch (Exception e){
+			} catch (Exception e) {
 				System.out.println(e);
 			}
-			System.out.println("Data Loaded.");			
+			System.out.println(Constants.loadData);			
 		}
 
-		else if(args[0].contains("+")){
-			System.out.println("Loading data ...");			
+		else if(args[0].contains(Constants.plus)) {
+			System.out.println(Constants.loadingData);			
 			try {
-				BufferedWriter reader = new BufferedWriter(new FileWriter("students.txt", true));
+				BufferedWriter reader = write();
 			    String student = args[0].substring(1);
 				Date date = new Date();
-	            String dateFormatString = "dd/mm/yyyy-hh:mm:ss a";
+	            String dateFormatString = Constants.dateFormate;
 				DateFormat dateFormat = new SimpleDateFormat(dateFormatString);
 	            String formatDate= dateFormat.format(date);
-			    reader.write(", "+student+"\nList last updated on "+formatDate);
+			    reader.write(Constants.comma + " " + student + Constants.lastupdate + formatDate);
 		     	reader.close();
-			} catch (Exception e){
+			} catch (Exception e) {
 				System.out.println(e);
 			}
 							
-			System.out.println("Data Loaded.");	
+			System.out.println(Constants.loadData);	
 		}
-		else if(args[0].contains("?")) 
-		{
-			System.out.println("Loading data ...");			
+		else if(args[0].contains(Constants.question)) {
+			System.out.println(Constants.loadingData);			
 			try {
-				BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt")));
+				BufferedReader reader = read();
 			    String allStudents = reader.readLine();
-			    String names[] = allStudents.split(",");
+			    String names[] = allStudents.split(Constants.comma);
 			    boolean done = false;
 		     	String student = args[0].substring(1);
 			    for(int idx = 0; idx<names.length && !done; idx++) {
 					if(names[idx].equals(student)) {
-					System.out.println("We found it!");
+					System.out.println(Constants.found);
 					done=true;
 					}
 				}
-			  } catch (Exception e){
+			  } catch (Exception e) {
 				System.out.println(e);
 			}
-			System.out.println("Data Loaded.");				
+			System.out.println(Constants.loadData);				
 		}
-		else if(args[0].contains("c")) 
-		{
-			System.out.println("Loading data ...");			
+		else if(args[0].contains(Constants.c)) {
+			System.out.println(Constants.loadingData);			
 			try {
-				BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt")));
+				BufferedReader reader = read();
 		     	String students = reader.readLine();
 			    char names[] = students.toCharArray();
 			    boolean in_word = false;
 			    int count=0;
 			    for(char name:names) {
-					if(name ==' ')
-					{
+					if(name ==' ') {
 					if (!in_word) {
 						count++;
 						in_word =true;
@@ -97,11 +93,39 @@ public class StudentList {
 				    	}
 			    	}
 		    	}
-			    System.out.println(count +" word(s) found " + names.length);
-			} catch (Exception e){
+			    System.out.println(count + Constants.wordFound + names.length);
+			} catch (Exception e) {
 				System.out.println(e);
 			}
-			System.out.println("Data Loaded.");				
+			System.out.println(Constants.loadData);				
 		}
+
+
+	}
+
+	public static BufferedReader read() throws FileNotFoundException {
+
+		BufferedReader bufferedReader = new BufferedReader(
+			new InputStreamReader(
+				new FileInputStream(Constants.studentList)));
+		
+		
+		return bufferedReader;
+
+
+	}
+
+
+	public static BufferedWriter write() throws IOException {
+
+		BufferedWriter bufferedWriter = new BufferedWriter(
+				new FileWriter(Constants.studentList, true));
+		
+		
+		return bufferedWriter;
+
+
 	}
 }
+
+
